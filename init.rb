0a9829ff3f9bfb20bc2251c9a4a04f4cc009720b
lest <just.lest@gmail.com>
2009-07-21 16:46:15 +0300

    show only completed versions in select tags

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+require 'twinslash_form_options_helper_patch'
+
+Redmine::Plugin.register :redmine_twinslash do
+  name 'Redmine Twinslash plugin'
+  author 'Just Lest'
+  description ''
+  version '0.0.1'
+end
